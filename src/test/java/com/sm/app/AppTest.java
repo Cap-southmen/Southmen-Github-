6425a02ab38885c76e942eb3a2df6aeaa4995ba6
umar <umar@gmail.com>
2019-10-24 10:00:27 +0000

    updated code

---
+++ src/test/java/com/sm/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.sm.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
